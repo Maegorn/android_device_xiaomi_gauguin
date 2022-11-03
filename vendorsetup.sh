86c41734b72bf44debd453761e4f34a6480002da
Molyuu 摸鱼 <102394810+Molyuu@users.noreply.github.com>
2022-11-03 11:46:36 +0800

    gauguin: Add clang-crepuscular to sync script

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,21 @@
 echo 'Starting to clone stuffs needed for your device'
 
-echo 'Cloning Vendor tree [1/4]'
+echo 'Cloning Vendor tree [1/5]'
 # Vendor Tree
 git clone --depth=1 https://github.com/iitzrohan/vendor_xiaomi_gauguin.git vendor/xiaomi/gauguin
 
-echo 'Cloning Kernel tree [2/4]'
+echo 'Cloning Kernel tree [2/5]'
 # Kernel Tree
 git clone --depth=1 https://github.com/Molyuu/kernel_xiaomi_gauguin.git kernel/xiaomi/gauguin
 
-echo 'Cloning Xiaomi sdm845 Vendor tree [3/4]'
+echo 'Cloning Xiaomi sdm845 Vendor tree [3/5]'
 # Xiaomi SDM845 Vendor tree
 git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sdm845-common-extra vendor/xiaomi/sdm845-common-extra
 
-echo 'Cloning Xiaomi Hardware [4/4]'
+echo 'Cloning Xiaomi Hardware [4/5]'
 # Xiaomi hardware
 git clone --depth=1 https://github.com/pixelExperience/hardware_xiaomi hardware/xiaomi
+
+echo 'Cloning Crepuscular Clang [5/5]'
+git clone https://gitlab.com/endCredits/clang-crepuscular prebuilts/clang/host/linux-x86/clang-crepuscular --depth=1
 echo 'Completed, Now proceeding to lunch'
